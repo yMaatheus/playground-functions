d768948c94c2c951106ba71f5bb09a135a80b84c
Matheus <stvgamer74@gmail.com>
2021-12-02 18:24:17 -0300

    Requisitos 6 e 7 feitos

--- src/challenges.js
+++ src/challenges.js
@@ -13,12 +13,12 @@ function calcArea(base, height) {
 
 // Desafio 3
 function splitSentence(text) {
-  return text.split(" ");
+  return text.split(' ');
 }
 
 // Desafio 4
 function concatName(array) {
-  return array[array.length - 1] + ", " + array[0]; 
+  return array[array.length - 1] + ', ' + array[0];
 }
 
 // Desafio 5
@@ -28,13 +28,33 @@ function footballPoints(wins, ties) {
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+/*
+Nessa função foi utilizado o 'spred operator' consultado no artigo abaixo:
+Link: https://www.horadecodar.com.br/2021/06/29/encontrar-maior-e-menor-numero-de-array-em-javascript
+*/
+function highestCount(array) {
+  let higherNumber = Math.max(...array);
+  console.log(higherNumber);
+  let count = 0;
+  for (let index of array) {
+    if (index === higherNumber) {
+      count = +1;
+    }
+  }
+  return count;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanceMouseCat1 = Math.abs(mouse - cat1);
+  let distanceMouseCat2 = Math.abs(mouse - cat2);
+  if (distanceMouseCat1 < distanceMouseCat2) {
+    return 'cat1';
+  } else if (distanceMouseCat2 < distanceMouseCat1) {
+    return 'cat2';
+  } else if (distanceMouseCat1 === distanceMouseCat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
