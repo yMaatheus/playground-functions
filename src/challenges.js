8bb11054c713687ada916c98230d58a96af130c8
Matheus <stvgamer74@gmail.com>
2021-12-02 15:16:26 -0300

    Requisito 2 feito

--- src/challenges.js
+++ src/challenges.js
@@ -7,8 +7,9 @@ function compareTrue(arg1, arg2) {
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  return (base * height) / 2;
 }
 
 // Desafio 3
