9a2eb58f2fae348b9a49e747decfeeecf365e972
Matheus <stvgamer74@gmail.com>
2021-12-12 17:23:23 -0300

    Retirando console.log do codigo :)

--- src/challenges2.js
+++ src/challenges2.js
@@ -40,8 +40,6 @@ function generatePhoneNumber(array) {
   return number;
 }
 
-console.log(generatePhoneNumber([1, 1, 12, 4, 5, 6, 7, 8, 9, 0, 1]));
-
 // Desafio 12
 function triangleCheck() {
   // seu código aqui
